f007144609b1e07c7f375b6a597ece8c036b09ed
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-06-27 20:08:40 +0700

    feat: update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { MessageDto } from './dtos/message.dto';
@@ -6,6 +6,11 @@ import { MessageDto } from './dtos/message.dto';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return 'Welcome to the RabbitMQ Consumer for You App RESTful API! - made by @ezralazuardy.';
+  }
 
   @EventPattern('message_sent')
   async handleUserMessage(@Payload() messageDto: MessageDto) {
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,51 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  // create the microservice
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  // create the app
+  const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [process.env.RABBITMQ_URL],
-        queue: 'messages-queue',
-        queueOptions: {
-          durable: false,
-        },
-      },
-    },
+    new FastifyAdapter(),
   );
 
+  // start listening for shutdown hooks
+  app.enableShutdownHooks();
+
+  // set the rabbitmq microservice
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL],
+      queue: 'messages-queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
   // start the microservice
-  await app.listen();
+  await app.startAllMicroservices();
+
+  // start the app
+  await app.listen(8000, '0.0.0.0');
+
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.RMQ,
+  //     options: {
+  //       urls: [process.env.RABBITMQ_URL],
+  //       queue: 'messages-queue',
+  //       queueOptions: {
+  //         durable: false,
+  //       },
+  //     },
+  //   },
+  // );
 }
 bootstrap();
